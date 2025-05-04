cd5271ed9930ef080f8fb20cf4c08575e9221e07
Tariq Shehzad Siddiqui <139456396+123Tariq123@users.noreply.github.com>
2025-05-04 11:38:39 +0500

    Add files via upload

---
+++ Tariq 2nd April.py
@@ -0,0 +1,52 @@
+
+# name="Tariq"
+# age=65
+# if age < 40:
+#     print("You are young")
+# elif age > 45:
+#     print("You need to exercise")
+# else:
+#     print("You are mature now")
+
+# student1_score=75
+# student2_score=80
+# student3_score=85
+# student4_score=90
+# student5_score=96
+# student6_score=15
+# student7_score=45
+# student8_score=100
+
+# if student_score < 20:
+#     print("i")
+# elif student_score < 45:
+#     print("f")
+# elif student_score > 77:
+#     print("b")
+# else:
+#     print("Score is between 45 and 77")
+
+
+print("Hello World!")
+(f"ASA,Eric Would you like to Learn some python today")
+name=("Moosa")
+
+print("Hello ", "would you like to learn some Python today?")
+print("Hello" , "would you like to learn some Python today")
+print("Hello ", "would you like to learn some Python today?")
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+# console.log("Hello World!");
+# console.log("ASA Eric Would you like to Learn some python today");
+# var name2 = "Moosa";
+# console.log("Hello ".concat(name2.toLowerCase(), ", would you like to learn some Python today?"));
+# console.log("Hello ".concat(name2.toUpperCase(), ", would you like to learn some Python today?"));
+# console.log("Hello ".concat(toTitleCase(name2), ", would you like to learn some Python today?"));
+# function toTitleCase(str) {
+#     return str.replace(/\w\S*/g, function (txt) {
+#         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+#     });
+# }
